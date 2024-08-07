681d641d86f7de0d1e03e4c5b18d3ba6ac6d0761
Delmar Filho <ddlmar13@gmail.com>
2024-08-07 14:10:07 -0300

    feat :sparkles: added integration with sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,19 @@ import { AppService } from './app.service';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
 
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [ReportsModule, UsersModule],
+  imports: [
+    ReportsModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
